66e9beeed5d0c156b83729edea309f7c1e7a2e5d
aagp <bearz_x@hotmail.com>
2014-04-25 12:37:46 -0700

    SIIReportes v4.0 Final
    Aplicación Terminada

--- SIIReportes/SIIRRegistrarSolicitud/src/com/siireportes/presentacion/Registrar.java
+++ SIIReportes/SIIRRegistrarSolicitud/src/com/siireportes/presentacion/Registrar.java
@@ -91,8 +91,9 @@ public class Registrar extends javax.swing.JFrame {
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("SIIR - Registrar Solicitud");
+        setAlwaysOnTop(true);
         setIconImage(iconSiir());
         setName("SIIRRegistrarSolicitud"); // NOI18N
         setResizable(false);
